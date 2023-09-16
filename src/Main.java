66455d155aca85746479bbc0e7988f06049b3758
De93m <DmitryKorshunov93@gmail.com>
2023-09-16 16:01:40 +0300

    доделал

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,83 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание №1");
-        int[] i = new int[3];
-        i[0] = 1;
-        i[1] = 2;
-        i[2] = 3;
+        int[] whole = new int[3];
+        for (int i = 0; i < whole.length ; i++) {
+            whole [i] = i +1;
 
-        double[] d = {1.57, 7.654, 9.986};
+        }
 
-        int[] b = {2, 6, 8, 10, 15};
+        double[] fractional = {1.57, 7.654, 9.986};
+
+        int[] myNumbers = {2, 6, 8, 10, 15};
 
         System.out.println("Задание №2");
 
-        System.out.println(i[0] + ", " + i[1] + ", " + i[2]);
-        System.out.println(d[0] + ", " + d[1] + ", " + d[2]);
-        System.out.print(b[0] + ", " + b[1] + ", " + b[2] + ", " + b[3] + ", " + b[4]);
-
+        for (int i = 0; i < whole.length; i++) {
+            if (i == whole.length - 1) {
+                System.out.print(whole[i]);
+                break;
+            }
+            System.out.print(whole[i] + ", ");
+        }
         System.out.println();
 
+        for (int i = 0; i < fractional.length; i++) {
+            if (i == fractional.length - 1) {
+                System.out.print(fractional[i]);
+                break;
+            }
+            System.out.print(fractional[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 0; i < myNumbers.length; i++) {
+            if (i == myNumbers.length - 1) {
+                System.out.print(myNumbers[i]);
+                break;
+            }
+            System.out.print(myNumbers[i] + ", ");
+        }
+        System.out.println();
         System.out.println("Задание №3");
 
-        System.out.println(i[2] + ", " + i[1] + ", " + i[0]);
-        System.out.println(d[2] + ", " + d[1] + ", " + d[0]);
-        System.out.print(b[4] + ", " + b[3] + ", " + b[2] + ", " + b[1] + ", " + b[0]);
+        for (int i = whole.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(whole[i]);
+                break;
+            }
+            System.out.print(whole[i] + ", ");
+        }
 
-        System.out.println();
+        for (int i = fractional.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(fractional[i]);
+                break;
+            }
+            System.out.print(fractional[i] + ", ");
+        }
+
+        for (int i = myNumbers.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(myNumbers[i]);
+                break;
+            }
+            System.out.print(myNumbers[i] + ", ");
+        }
 
         System.out.println("Задание №4");
-
-        for (int c = 0; c < i.length; c++)
-            if (i[c] % 2 != 0 ) {
-                i[c]++;
-                System.out.print(i[c]);
+        for (int i = 0; i < whole.length; i++) {
+            if (whole[i] % 2 == 1){
+                whole[i] ++;}
+            if (i == whole.length - 1) {
+                System.out.print(whole[i]);
+                break;
             }
+                System.out.print(whole[i] + ", ");
+        }
+
     }
 }
